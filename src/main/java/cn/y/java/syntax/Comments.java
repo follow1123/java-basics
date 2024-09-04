50c6bb60eb941a48d7f41c96c84e584180e5a5e6
yf <--->
2024-09-05 00:08:19 +0800

    init: java基础项目demo初始化

---
+++ src/main/java/cn/y/java/syntax/Comments.java
@@ -0,0 +1,19 @@
+package cn.y.java.syntax;
+
+/**
+ * 文档注释
+ * @author a
+ * @version 1.0
+ */
+public class Comments {
+
+    /*
+    多行注释
+    多行注释
+    多行注释
+    */
+    public static void main(String[] args) {
+        // 单行注释
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/cn/y/java/syntax/StringDataType.java
@@ -0,0 +1,18 @@
+package cn.y.java.syntax;
+
+public class StringDataType {
+    public static void main(String[] args) {
+        int i1 = 234;
+        float f1 = 34.55F;
+        boolean b1 = true;
+        String s1 = "str";
+
+        String s2 = s1 + i1; // str234
+        String s3 = s1 + f1; // str34.55
+        String s4 = s1 + b1; // strtrue
+
+        String s5 = i1 + f1 + s1; // 268.55str
+
+        String s6 = s1 + f1 + i1; // str34.55234
+    }
+}
